98a4297f1fe0f25c34f8adafde09a7f25673dba5
Paulo Baraldi Mausbach <pbmausbach@hotmail.com>
2019-02-15 00:35:19 -0200

    Add files via upload
    Arquivos do código unificado do programador e do gerenciador dos sensores do nó sensor da RSSF

---
+++ ROTA_Firmware/FOTA_config.h
@@ -0,0 +1,8 @@
+#define APL_FOTA_PROG 1
+#define APL_RADIUINO 0
+
+//Define this constant for Programmer devices
+#define FOTA_DEVICE_APL APL_FOTA_PROG  
+
+//Define this constant for Radiuino devices
+//#define FOTA_DEVICE_APL APL_RADIUINO
